1aa372c741f54a1ebc4d81d4858c9fedf7cafcfa
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2025-12-22 11:13:11 +0700

    Update

--- app/(public-pages)/lien-he/page.tsx
+++ app/(public-pages)/lien-he/page.tsx
@@ -97,20 +97,20 @@ const ContactPage = () => {
               </div>
             </div> */}
           </div>
-
-          <div>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d411.96735430105883!2d106.73352467476528!3d10.787086385437869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527287778aad7%3A0x9031be540b1c390!2zVklORkFTVCBMxq_GoE5HIMSQ4buKTkggQ-G7pkF8IFZJTkZBU1QgUVXhuqxOIDI!5e0!3m2!1svi!2s!4v1766245209002!5m2!1svi!2s"
-              className="w-full h-full max-[846px]:aspect-video rounded-md shadow-md"
-              loading="lazy"
-              allowFullScreen
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
-          </div>
         </div>
 
-        <div className="pt-16">
+        <div className="mt-10">
           <QuickConsultForm />
+        </div>
+
+        <div className="mt-14">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d411.96735430105883!2d106.73352467476528!3d10.787086385437869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527287778aad7%3A0x9031be540b1c390!2zVklORkFTVCBMxq_GoE5HIMSQ4buKTkggQ-G7pkF8IFZJTkZBU1QgUVXhuqxOIDI!5e0!3m2!1svi!2s!4v1766245209002!5m2!1svi!2s"
+            className="w-full aspect-video rounded-md shadow-md"
+            loading="lazy"
+            allowFullScreen
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
     </div>
